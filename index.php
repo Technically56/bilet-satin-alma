e16038a4089312945ba4bc1ff189a5f2b33fb041
Technically56 <pusatdincer@gmail.com>
2025-10-05 22:53:21 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+<?php include 'includes/navbar.php'; ?>  <!-- <--- this adds the menu here -->
+
+<h1>Welcome to MySite</h1>
+
+</body>
+</html>
