4641aaddeaf1c60a9e2f5460b730a2f81d284c52
Technically56 <pusatdincer@gmail.com>
2025-10-06 00:50:05 +0300

    created navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,14 +1,40 @@
-<!-- nav.php -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">MySite</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-      <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
-      <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
-    </ul>
+<nav class="navbar navbar-expand-sm navbar-dark bg-primary shadow fs-5">
+  <div class="container-fluid">
+    <a href="#" class="navbar-brand mb-0 h1 fs-5">
+      <img 
+        class="d-inline-block align-center"
+        src="static/images/logo.png"
+        width="50" height="50"
+        alt="Logo"
+      />
+      Hızlı Bilet
+    </a>
+    <button type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Navigasyonu Aç" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item active">
+<!-- Add Dynamic Company Listing Here-->
+          <a href="#" class="nav-link">Firmalar</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Bilet Al</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">İletişim</a>
+        </li>
+        
+<!-- Add Login Check Here-->
+        <li class="nav-item">
+          <a href="profile.php" class="nav-link">
+            <img 
+              src="static/images/profilelogo.svg"
+              alt="Profile"
+              width="30" height="30"
+              class="rounded-circle"
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
