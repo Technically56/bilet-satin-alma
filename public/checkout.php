19c2c171f07a2352cd72fde10fd7af9c5c06dab6
Technically56 <pusatdincer@gmail.com>
2025-10-25 02:02:59 +0300

    minor bug fix

--- public/checkout.php
+++ public/checkout.php
@@ -225,7 +225,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             <?php
         endif;
+    else:
     endif;
-else:
     echo "<script>setTimeout(() => location.href = '/needlogin.php', 1000)</script>";
 endif; ?>
